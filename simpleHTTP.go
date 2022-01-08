276c4311c03a5b4f95973e5999a44a4ab9153085
Ravindramandada <96417132+Ravindramandada@users.noreply.github.com>
2022-01-08 22:09:45 +0530

    Update simpleHTTP.go

--- simpleHTTP.go
+++ simpleHTTP.go
@@ -16,21 +16,6 @@ func main() {
 
 // printing request headers/params
 func handler(w http.ResponseWriter, r *http.Request) {
-
-	log.Print("request from address: %q\n", r.RemoteAddr)
-	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-	}
-	fmt.Fprintf(w, "Host = %q\n", r.Host)
-	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
-	if err := r.ParseForm(); err != nil {
-		log.Print(err)
-	}
-	for k, v := range r.Form {
-		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
-	}
-	fmt.Fprintf(w, "\n===> local IP: %q\n\n",GetOutboundIP())
 	fmt.Fprintf(w, "Welcome message = %s\n", os.Getenv("message"))
 }
 
